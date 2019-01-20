c76b68ff893388494f7829d6cfef140d2158b168
Manuel J. Bernal <manu.asi2@gmail.com>
2019-01-20 02:19:33 +0100

    Improved script

--- makefile.sh
+++ makefile.sh
@@ -4,12 +4,12 @@ command -v npm >/dev/null 2>&1 ||  echo >&2 "I require npm but it's not installe
 echo "Removing old binary file"
 rm -f bin/index-linux
 echo "Copying package.json to lib/package.json"
-cp package.json lib/package.json
 echo "Generating code to ECMAScript 5"
-npm run build
+echo `pwd`
+echo `npm run build`
 echo "Copying package.json to dist/package.json"
-cp package.json dist/package.json
+echo `cp package.json dist/package.json`
 echo "Building binaries"
-pkg -t node10-linux-x64 dist/index.js -o ./bin/index-linux
-pkg -t node10-win-x64 dist/index.js -o ./bin/index-win
+echo `pkg -t node10-linux-x64 dist/index.js -o ./bin/index-linux`
+echo `pkg -t node10-win-x64 dist/index.js -o ./bin/index-win`
 echo "You can find the binary generated under bin/ directory"
